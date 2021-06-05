35e5f87317b32e12a83a0ebffeb0a1c0f12f2c33
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-06-05 17:09:37 +0200

    Script update

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ $(document).ready(function() {
             title: taskTitle,
             content: taskContent
          }),
-         success: getAllTasks();
+         success: getAllTasks()
       });
    }
 
